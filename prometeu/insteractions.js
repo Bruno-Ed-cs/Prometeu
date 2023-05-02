43ddc378e57e577c04aef8f6b5260a98df6b06dd
Bruno <brunosilva.pinf@gmail.com>
2023-05-02 16:18:09 -0300

    advance

--- prometeu/insteractions.js
+++ prometeu/insteractions.js
@@ -1,59 +1,18 @@
-
-function call_aside(){
-    let window = document.getElementById('side_menu').style.display
+var ui_menus = document.querySelectorAll('.menu');
 
 
-    if (window == 'flex') {
-        document.getElementById('side_menu').style.display = 'none'
-
-    } else{
-        document.getElementById('side_menu').style.display = 'flex'
-        document.getElementById('config_menu').style.display = 'none'
-        document.getElementById('bar').style.display = 'none'
-
-    }
-
-}
-
-function call_bar(){
-    let window_2 = document.getElementById('bar').style.display
-
-
-    if (window_2 == 'flex') {
-        document.getElementById('bar').style.display = 'none'
-
-    } else{
-        document.getElementById('bar').style.display = 'flex'
-        document.getElementById('side_menu').style.display = 'none'
-        document.getElementById('config_menu').style.display = 'none'
-
-    }
-
-}
-
-function call_config(){
-    let window_3 = document.getElementById('config_menu').style.display
-
-
-    if (window_3 == 'flex') {
-        document.getElementById('config_menu').style.display = 'none'
-
-    } else{
-        document.getElementById('config_menu').style.display = 'flex'
-        document.getElementById('side_menu').style.display = 'none'
-        document.getElementById('bar').style.display = 'none'
-
-    }
-
-}
+function none_menu(exeptions)
+for (let n = 0; n < ui_menus.length; n++){
+    ui_menus[n].style.display = 'none';
+};
 
 function call(target_id){
     document.getElementById(target_id).style.display = "flex"
     console.log(target_id)
-}
+};
 
 function close_element(parent_id){
     document.getElementById(parent_id).style.display = "none"
     console.log(parent_id)
-}
+};
 
