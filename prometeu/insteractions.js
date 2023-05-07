a51a0a669b3ba2f3eb6e49bb2c8573104b4372cf
Bruno-Ed-cs <brunosilva.pinf@gmail.com>
2023-05-06 22:37:39 -0300

    theme update

--- prometeu/insteractions.js
+++ prometeu/insteractions.js
@@ -5,22 +5,34 @@ var cur_theme = 'light_theme';
 var theme_buttons = document.getElementsByClassName("field");
 var sel_theme_button = localStorage.getItem('sel_theme_button');
 
-localStorage.setItem('sel_theme_button', sel_theme_button)
-
-var themes = {dark_theme:
+var themes = {
+  
+              dark_theme:
               [['--Primary_color', '#730099'],
               ['--Secundary_color', '#7ab10e'],
               ['--Hover_color', '#a6db3bab'],
               ['--Background_color', '#413f3f'],
               ['--Primary_font_color', '#FFFFFF'],
-              ['--Secundary_font_color', '#ffffff']],
+              ['--Secundary_font_color', '#ffffff'],
+              ['--Shadow_color', 'rgba(0, 0, 0, 0.2)']],
+              
               light_theme:
               [['--Primary_color', '#EE8B31'],
               ['--Secundary_color', '#EE3136'],
               ['--Hover_color', '#f72d34a2'],
               ['--Background_color', '#ffffff'],
               ['--Primary_font_color', '#FFFFFF'],
-              ['--Secundary_font_color', '#000000']]
+              ['--Secundary_font_color', '#000000'],
+              ['--Shadow_color', 'rgba(0, 0, 0, 0.2)']],
+
+              high_contrast:
+              [['--Primary_color', '#000000'],
+              ['--Secundary_color', '#f2ff00'],
+              ['--Hover_color', '#fbff00dc'],
+              ['--Background_color', '#000000'],
+              ['--Primary_font_color', '#FFFFFF'],
+              ['--Secundary_font_color', '#FFFFFF'],
+              ['--Shadow_color', 'rgba(255, 255, 255, 0.2)']]
             };
 
 // console.log(root_style);
@@ -39,18 +51,21 @@ if (localStorage.getItem('cur_theme') === null){
 
   
 if (localStorage.getItem('sel_theme_button') === null){
+  console.log(localStorage.getItem('sel_theme_button'))
   localStorage.setItem('sel_theme_button', 'light');
 
 } else{
   for (i=0; i < theme_buttons.length; i++){
-    if (theme_buttons[i].id == sel_theme_button){
+    if (theme_buttons[i].id === sel_theme_button){
       
       theme_buttons[i].classList.add('selected_field');
     } else{
       theme_buttons[i].classList.remove('selected_field');
+      console.log(theme_buttons[i].classList)
     }
     console.log(theme_buttons[i].classList)
   };
+  console.log(localStorage.getItem('sel_theme_button'))
 };
 
 document.addEventListener('click', function (event){
