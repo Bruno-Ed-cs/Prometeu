07b5dc635c0e7b5e3568f33c0ba7b2bf1d791f62
Bruno-Ed-cs <brunosilva.pinf@gmail.com>
2025-04-24 11:53:08 -0300

    it works

--- www/connect.php
+++ www/connect.php
@@ -1,7 +1,7 @@
 
 <?php
 
-$mysqli = new mysqli("localhost", "user", "12345", "Prometeudb", 3306);
+$mysqli = new mysqli("db", "user", "12345", "Prometeudb", 3306);
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
